523af69771ced681400214de3ff547645a90ebe2
Vladimir Fomene <vladimirfomene@gmail.com>
2018-12-18 09:49:11 +0100

    show usage for @Getter and @Setter

--- src/main/java/Accessor/LombokAppUser.java
+++ src/main/java/Accessor/LombokAppUser.java
@@ -1,4 +1,18 @@
 package Accessor;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class LombokAppUser {
+
+    @Setter @Getter
+    private String username;
+
+    @Setter @Getter
+    private String password;
+
+    public LombokAppUser(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
--- src/main/java/Accessor/VanillaAppUser.java
+++ src/main/java/Accessor/VanillaAppUser.java
@@ -1,4 +1,29 @@
 package Accessor;
 
 public class VanillaAppUser {
+
+    private String username;
+
+    private String password;
+
+    public VanillaAppUser(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
